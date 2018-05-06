d94d701bbca2cac97b26de6d93e4449f9915ffdf
Harmen Janssen <harmen@grrr.nl>
2018-05-06 09:23:02 +0200

    Functional patterns

--- chapter07/exercises.hs
+++ chapter07/exercises.hs
@@ -31,3 +31,30 @@ nums x =
         GT -> 1
         EQ -> 0
 
+dodgy :: Num a => a -> a -> a
+dodgy x y = x + y * 10
+
+oneIsOne :: Num a => a -> a
+oneIsOne = dodgy 1
+
+oneIsTwo :: Num a => a -> a
+oneIsTwo = (flip dodgy) 2
+
+myAbs :: Integer -> Integer
+myAbs x
+    | x < 0 = (-x)
+    | otherwise = x
+
+avgGrade :: (Fractional a, Ord a) => a -> Char
+avgGrade x
+    | y >= 0.9 = 'A'
+    | y >= 0.8 ='B'
+    | y >= 0.7 ='C'
+    | y >= 0.59 = 'D'
+    -- | y < 0.59 = 'F'
+    | otherwise = 'F'
+    where y = x / 100
+
+pal' xs
+    | xs == reverse xs = True
+    | otherwise = False
