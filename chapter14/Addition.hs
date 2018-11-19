add31ef5d4db887c279e085bb2e42801a0a84f29
Harmen Janssen <harmen@grrr.nl>
2018-11-19 06:24:13 +0100

    Add property test

--- chapter14/Addition.hs
+++ chapter14/Addition.hs
@@ -1,9 +1,17 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
+    describe "addition" $ do
+        it "1 + 1 is greater than 1" $ do
+            (1 + 1) > 1 `shouldBe` True
+        it "2 + 2 is equal to 4" $ do
+            (2 + 2) `shouldBe` 4
+        it "x + 1 is always greater than x" $ do
+            property $ \x -> x + 1 > (x :: Int)
     describe "dividedBy" $ do
         it "15 divided by 3 is 5" $ do
             dividedBy 15 3 `shouldBe` (5, 0)
